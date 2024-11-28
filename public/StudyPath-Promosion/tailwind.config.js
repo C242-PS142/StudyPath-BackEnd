efd35e1b750ced640193f079313cc93c5f03ffe2
Rio <rioaditya1605@gmail.com>
2024-11-28 22:28:35 +0700

    Update Web Promosion

---
+++ public/StudyPath-Promosion/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/line-clamp')],
+}
+
+module.exports = {
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        scrollBehavior: ['responsive'],
+      },
+    },
+    plugins: [],
+  };
