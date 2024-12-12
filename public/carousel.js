e83fce3541d4f6736e6114e70c287169287d6de2
Johanes Nicky C. S <apanyaclay1@gmail.com>
2024-12-12 23:03:02 +0700

    Fix

--- public/carousel.js
+++ public/carousel.js
@@ -9,3 +9,12 @@ prevButton.addEventListener("click", () => {
 nextButton.addEventListener("click", () => {
   carousel.scrollBy({ left: carousel.offsetWidth, behavior: "smooth" });
 });
+
+function downloadFile() {
+  const link = document.createElement('a');
+  link.href = '/public/uploads/StudyPath v1.0.apk';
+  link.download = 'StudyPath v1.0.apk'; // Optional: you can specify the filename here
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
